d94a7886fc36a69b7683012f189322ecfb3088da
Aylein <shikinami.asuka@foxmail.com>
2014-11-13 23:24:24 +0900

    the sql file

--- index.php
+++ index.php
@@ -5,23 +5,12 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Index</title>
         <link rel="stylesheet" href="styles/style.css" />
-        <!--[if IE 7]>
-        <style type="text/css">
-            /*ie6 fix顶端元素*/
-            .header { top:expression(eval(document.documentElement.scrollTop)); }
-        </style>
-        <![endif]-->
     </head>
     <body>
         <section class="header">
             <div class="myname">
                 <div class="master"><img src="images/master.jpg"/><span>&nbsp;The 4th AyleinOter Was Here &nbsp;</span></div>
                 <div class="menu">
-                    <span>打铁的汉子</span>&nbsp;&nbsp;
-                    <span>你往哪跑</span>&nbsp;&nbsp;
-                    <span>快回来</span>&nbsp;&nbsp;
-                    <span>不要跑</span>&nbsp;&nbsp;
-                    <span>决战到天亮</span>
                 </div>
                 <div class="clear"></div>
             </div>
