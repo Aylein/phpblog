4621ac060d1de409b1b9f2871a5f6e82d5a07caf
ls <2667146298@qq.com>
2014-11-14 11:36:40 +0800

    tables

--- date/tables.sql
+++ date/tables.sql
@@ -1,3 +1,34 @@
-create database if not exists php;
-
 use php;
+
+create table if not exists Types (
+    typeid int primary key auto_increment,
+    typepid int default 0,
+    typename nvarchar(15) not null,
+    typesort int default 0,
+    typevalid int default 1
+)auto_increment = 12580;
+
+create table if not exists Documents (
+	docid int primary key auto_increment,
+	typeid int references Types(typeid) on delete cascade on update cascade,
+    docpid int default 0,
+    doctitle nvarchar(15) not null,
+    docsubtitle nvarchar(50) default "",
+    doccontent nvarchar(12000) not null,
+    docview int default 0,
+    doccreatetime timestamp default current_timestamp,
+    docupdatetime timestamp default current_timestamp on update current_timestamp,
+    docsort int default 0,
+    docvalid int default 1
+)auto_increment = 12315;
+
+create table if not exists Comments (
+	comid int primary key auto_increment,
+	docid int references Documents(docid) on delete cascade on update cascade,
+    compid int default 0,
+    comname nvarchar(12) not null,
+    comdate timestamp default current_timestamp,
+    comment nvarchar(450) not null,
+    comsort int default 0,
+    comvalid int default 1
+)auto_increment = 12306;
